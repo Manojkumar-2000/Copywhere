fdb7756a91f32f5b6910879be3624630f1ad2dda
Manojkumar-2000 <manojwppp@gmail.com>
2021-04-13 12:23:29 +0530

    Merge branch 'main' of https://github.com/Manojkumar-2000/Copywhere

--- app/src/main/java/com/miniproject/copywhere/MainActivity.java
+++ app/src/main/java/com/miniproject/copywhere/MainActivity.java
@@ -1,14 +1,70 @@
 package com.miniproject.copywhere;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Uri image_uri;
+    ImageButton Select_image,exit,Process_image;
+    ImageView selected_image_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Select_image = findViewById(R.id.add_image);
+        exit = findViewById(R.id.exit);
+        Process_image = findViewById(R.id.Process);
+        selected_image_view = findViewById(R.id.selected_image);
+
+        Select_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Selectimage();
+            }
+        });
+        Process_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                image_uri = result.getUri();
+                selected_image_view.setImageURI(image_uri);
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+
+    private void Selectimage() {
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(this);
     }
 }
---
+++ app/src/main/java/com/miniproject/copywhere/resultactivity.java
@@ -0,0 +1,13 @@
+package com.miniproject.copywhere;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class resultactivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+    }
+}
